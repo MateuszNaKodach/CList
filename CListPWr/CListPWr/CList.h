a9001cce8d5041a181e49e42f39f1ee32443ab73
Mateusz Nowak <nmateusz96@gmail.com>
2017-01-21 18:53:41 +0100

    Initializer list added to CList constructor

--- CListPWr/CListPWr/CList.h
+++ CListPWr/CListPWr/CList.h
@@ -7,13 +7,14 @@ class Node
 {
 private:
 	T value;
-	Node* previous = nullptr;
-	Node* next = nullptr;
+	Node* previous=nullptr;
+	Node* next=nullptr;
 
 public:
 	Node(T value);
 	Node(Node &pcOther);
 	T getValue();
+	void setValue(T value);
 	void setNext(Node* next);
 	void setPrevious(Node* previous);
 	Node* getNext();
@@ -25,15 +26,144 @@ template <class T>
 class CList
 {
 private:
-	Node<T>* head;
+	Node<T>* head=nullptr;
 	int size=0;
 
 public:
-	//CList();
-	//CList(std::initializer_list<T> vInitList);
+	CList();
+	CList(std::initializer_list<T> vInitList);
 	int getSize() const;
-	T getNodeValue(int index);
+	T get(int index);
 	void add(T value);
+	void add(T value, int index)=delete;
+	void set(int index, T value);
 };
 
 
+template<class T>
+Node<T>::Node(T value) : value(value)
+{
+}
+
+template<class T>
+Node<T>::Node(Node & pcOther)
+{
+	Node<T> *pc_res = new Node<T>(pcOther.getValue());
+
+	pc_res->setNext(new Node<T>(*pcOther.getNext()));
+	pc_res->setPrevious(new Node<T>(*pcOther.getPrevious()));
+
+	return(*pc_res);
+}
+
+template<class T>
+T Node<T>::getValue()
+{
+	return value;
+}
+
+template<class T>
+inline void Node<T>::setValue(T value)
+{
+	this->value = value;
+}
+
+template<class T>
+void Node<T>::setNext(Node * next)
+{
+	this->next = next;
+}
+
+template<class T>
+void Node<T>::setPrevious(Node * previous)
+{
+	this->previous = previous;
+}
+
+template<class T>
+Node<T> * Node<T>::getNext()
+{
+	return next;
+}
+
+template<class T>
+Node<T> * Node<T>::getPrevious()
+{
+	return previous;
+}
+
+template<class T>
+Node<T>::~Node()
+{
+	delete previous;
+	delete next;
+}
+
+
+template <class T>
+CList<T>::CList():head(nullptr),size(0)
+{
+}
+
+template<class T>
+inline CList<T>::CList(std::initializer_list<T> vInitList)
+{
+	for(const T *pi=vInitList.begin();pi!=vInitList.end();++pi)
+		add(*pi);
+}
+
+template<class T>
+int CList<T>::getSize() const
+{
+	return size;
+}
+
+template<class T>
+T CList<T>::get(int index)
+{
+	if (index > size - 1)
+		throw std::out_of_range("Current index: " + std::to_string(index) + "Current size: " + std::to_string(size));
+
+	Node<T>* currentNode = head;
+
+	for (int i = 0; i != index; i++)
+		currentNode = currentNode->getNext();
+
+	return (currentNode->getValue());
+}
+
+template<class T>
+void CList<T>::add(T value)
+{
+	Node<T> *node = new Node<T>(value);
+	if (head==nullptr) {
+		head = node;
+	}
+	else
+	{
+		Node<T> *currentNode = head;
+		while (currentNode->getNext()!=nullptr)
+			currentNode = currentNode->getNext();
+
+		currentNode->setNext(node);
+		node->setPrevious(currentNode);
+	}
+
+	size++;
+}
+
+
+
+template<class T>
+void CList<T>::set(int index, T value)
+{
+	if (index > size - 1)
+		throw std::out_of_range("Current index: " + std::to_string(index) + "Current size: " + std::to_string(size));
+
+	Node<T>* currentNode = head;
+
+	for (int i = 0; i != index; i++)
+		currentNode = currentNode->getNext();
+
+	currentNode->setValue(value);
+}
--- CListPWr/CListPWr/CListPWr.cpp
+++ CListPWr/CListPWr/CListPWr.cpp
@@ -3,15 +3,23 @@
 
 
 #include "CList.h"
-#include "CList.cpp"
 #include <iostream>
 
 int main()
 {
-	CList<int> *clist = new CList<int>();
+	CList<int> *clist = new CList<int>{1,2,3};
+	//CList<int> *clist = new CList<int>();
 
-	clist->add(10);
-	//std::cout << clist->getNodeValue(0);
+	//clist->add(10);
+	//clist->add(20);
+	std::cout << clist->get(0)<< std::endl;
+	std::cout << clist->get(1)<< std::endl;
+	try {
+		std::cout << clist->get(10);
+	}catch(std::out_of_range){
+		std::cout << "Oooo, nie ladnie! Wylazles poza zakres listy!"<< std::endl;
+	}
+	
 	system("pause");
     return 0;
 }
